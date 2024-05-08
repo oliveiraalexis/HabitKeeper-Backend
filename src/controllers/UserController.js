3feedd77119be71be5e328f5c1961abf95a2f5fa
Alexis Oliveira <alexis.silvaoliveira@gmail.com>
2024-05-07 22:52:39 -0300

    Creating new routes

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,8 +1,13 @@
 import User from '../models/User.js'
+import Habit from '../models/Habit.js'
 
 const getUsers = async (req, res) => {
     const users = await User.find()
     return res.status(200).json(users)
+}
+const getUser = async (req, res) => {
+    const user = await User.findById(req.params.userId)
+    return res.status(200).json(user)
 }
 
 const createUser = async (req, res) => {
@@ -11,4 +16,15 @@ const createUser = async (req, res) => {
     return res.status(201).json(newUser)
 }
 
-export {getUsers, createUser}
+const deleteUser = async (req, res) => {
+    const deletedUser = await User.deleteOne({_id: req.params.userId})
+    if (deletedUser.deletedCount == 1) 
+    {
+        await Habit.deleteMany({user_id: req.params.userId})
+        const users = await User.find()
+        return res.status(200).json(users)
+    } 
+    return res.status(204).json({retorno: 'Item não encontrado'})
+}
+
+export {getUser, getUsers, createUser, deleteUser}
