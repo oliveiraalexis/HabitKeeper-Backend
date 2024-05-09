6894c50837c6e760e614482233fa86eb22f32cbe
Alexis Oliveira <alexis.silvaoliveira@gmail.com>
2024-05-08 21:43:09 -0300

    Implementing try-catch

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,29 +2,46 @@ import User from '../models/User.js'
 import Habit from '../models/Habit.js'
 
 const getUsers = async (req, res) => {
-    const users = await User.find()
-    return res.status(200).json(users)
+    try {
+        const users = await User.find()
+        return res.status(200).json(users)
+    } catch(error){
+        return res.status(500).json(error.message)
+    }
 }
+
 const getUser = async (req, res) => {
-    const user = await User.findById(req.params.userId)
-    return res.status(200).json(user)
+    try{
+        const user = await User.findById(req.params.userId)
+        return res.status(200).json(user)
+    } catch(error){
+        return res.status(500).json(error.message)
+    }
 }
 
 const createUser = async (req, res) => {
-    const user = req.body
-    const newUser = await User.create(user)
-    return res.status(201).json(newUser)
+    try{
+        const user = req.body
+        const newUser = await User.create(user)
+        return res.status(201).json(newUser)
+    } catch(error) {
+        return res.status(500).json(error.message)
+    }
 }
 
 const deleteUser = async (req, res) => {
-    const deletedUser = await User.deleteOne({_id: req.params.userId})
-    if (deletedUser.deletedCount == 1) 
-    {
-        await Habit.deleteMany({user_id: req.params.userId})
-        const users = await User.find()
-        return res.status(200).json(users)
-    } 
-    return res.status(204).json({retorno: 'Item não encontrado'})
+    try{
+        const deletedUser = await User.deleteOne({_id: req.params.userId})
+        if (deletedUser.deletedCount == 1) 
+        {
+            await Habit.deleteMany({user_id: req.params.userId})
+            const users = await User.find()
+            return res.status(200).json(users)
+        } 
+        return res.status(204).json({retorno: 'Item não encontrado'})
+    } catch(error) {
+        return res.status(500).json(error.message)
+    }
 }
 
 export {getUser, getUsers, createUser, deleteUser}
