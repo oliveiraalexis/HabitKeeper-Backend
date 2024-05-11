d2c58ac755e1048473dafc5364ce291ca213c186
Alexis Oliveira <alexis.silvaoliveira@gmail.com>
2024-05-11 18:54:47 -0300

    Changing controllers to be classes

--- src/controllers/HabitController.ts
+++ src/controllers/HabitController.ts
@@ -1,52 +1,53 @@
 import Habit from '../models/Habit.js'
 import {Request, Response} from 'express'
 
-const getHabits = async (req: Request, res: Response) => {
-    try{
-        const habits = await Habit.find({user_id: req.params.userId})
-        return res.status(200).json(habits)
-    } catch(error: unknown) {
-        return res.status(500).json((error as Error).message)
+export class HabitController {
+
+    getHabits = async (req: Request, res: Response) => {
+        try{
+            const habits = await Habit.find({user_id: req.params.userId})
+            return res.status(200).json(habits)
+        } catch(error: unknown) {
+            return res.status(500).json((error as Error).message)
+        }
+    }
+    
+    getHabit = async (req: Request, res: Response) => {
+        try{
+            const habits = await Habit.find({_id: req.params.habitId, user_id: req.params.userId})
+            return res.status(200).json(habits)
+        } catch(error: unknown) {
+            return res.status(500).json((error as Error).message)
+        }
+    }
+    
+    createHabit = async (req: Request, res: Response) => {
+        try{
+            const habit = req.body
+            const newHabit = await Habit.create(habit)
+            return res.status(201).json(newHabit)
+        } catch(error: unknown) {
+            return res.status(500).json((error as Error).message)
+        }
+    }
+    
+    deleteHabits = async (req: Request, res: Response) => {
+        try{
+            const result = await Habit.deleteMany({user_id: req.params.userId})
+            if (result.deletedCount > 0) return res.status(200).json({retorno: 'Hábitos deletados'})
+            return res.status(404).json({retorno: 'Itens não encontrados'})
+        } catch(error: unknown) {
+            return res.status(500).json((error as Error).message)
+        }
+    }
+    
+    deleteHabit = async (req: Request, res: Response) => {
+        try{
+            const result = await Habit.deleteOne({_id: req.params.habitId})
+            if (result.deletedCount == 1) return res.status(200).json({retorno: 'Hábito deletado'})
+            return res.status(404).json({retorno: 'Hábito não encontrado'})
+        } catch(error: unknown) {
+            return res.status(500).json((error as Error).message)
+        }
     }
 }
-
-const getHabit = async (req: Request, res: Response) => {
-    try{
-        const habits = await Habit.find({_id: req.params.habitId, user_id: req.params.userId})
-        return res.status(200).json(habits)
-    } catch(error: unknown) {
-        return res.status(500).json((error as Error).message)
-    }
-}
-
-const createHabit = async (req: Request, res: Response) => {
-    try{
-        const habit = req.body
-        const newHabit = await Habit.create(habit)
-        return res.status(201).json(newHabit)
-    } catch(error: unknown) {
-        return res.status(500).json((error as Error).message)
-    }
-}
-
-const deleteHabits = async (req: Request, res: Response) => {
-    try{
-        const result = await Habit.deleteMany({user_id: req.params.userId})
-        if (result.deletedCount > 0) return res.status(200).json({retorno: 'Hábitos deletados'})
-        return res.status(404).json({retorno: 'Itens não encontrados'})
-    } catch(error: unknown) {
-        return res.status(500).json((error as Error).message)
-    }
-}
-
-const deleteHabit = async (req: Request, res: Response) => {
-    try{
-        const result = await Habit.deleteOne({_id: req.params.habitId})
-        if (result.deletedCount == 1) return res.status(200).json({retorno: 'Hábito deletado'})
-        return res.status(404).json({retorno: 'Hábito não encontrado'})
-    } catch(error: unknown) {
-        return res.status(500).json((error as Error).message)
-    }
-}
-
-export {getHabits, getHabit, createHabit, deleteHabits, deleteHabit}
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,47 +2,48 @@ import User from '../models/User.js'
 import Habit from '../models/Habit.js'
 import {Request, Response} from 'express'
 
-const getUsers = async (req: Request, res: Response) => {
-    try {
-        const users = await User.find()
-        return res.status(200).json(users)
-    } catch(error: unknown){
-        return res.status(500).json((error as Error).message)
-    }
-}
+export class UserController {
 
-const getUser = async (req: Request, res: Response) => {
-    try{
-        const user = await User.findById(req.params.userId)
-        return res.status(200).json(user)
-    } catch(error: unknown){
-        return res.status(500).json((error as Error).message)
-    }
-}
-
-const createUser = async (req: Request, res: Response) => {
-    try{
-        const user = req.body
-        const newUser = await User.create(user)
-        return res.status(201).json(newUser)
-    } catch(error: unknown) {
-        return res.status(500).json((error as Error).message)
-    }
-}
-
-const deleteUser = async (req: Request, res: Response) => {
-    try{
-        const deletedUser = await User.deleteOne({_id: req.params.userId})
-        if (deletedUser.deletedCount == 1) 
-        {
-            await Habit.deleteMany({user_id: req.params.userId})
+    getUsers = async (req: Request, res: Response) => {
+        try {
             const users = await User.find()
             return res.status(200).json(users)
-        } 
-        return res.status(404).json({retorno: 'Usuário não encontrado'})
-    } catch(error: unknown) {
-        return res.status(500).json((error as Error).message)
+        } catch(error: unknown){
+            return res.status(500).json((error as Error).message)
+        }
+    }
+    
+    getUser = async (req: Request, res: Response) => {
+        try{
+            const user = await User.findById(req.params.userId)
+            return res.status(200).json(user)
+        } catch(error: unknown){
+            return res.status(500).json((error as Error).message)
+        }
+    }
+    
+    createUser = async (req: Request, res: Response) => {
+        try{
+            const user = req.body
+            const newUser = await User.create(user)
+            return res.status(201).json(newUser)
+        } catch(error: unknown) {
+            return res.status(500).json((error as Error).message)
+        }
+    }
+    
+    deleteUser = async (req: Request, res: Response) => {
+        try{
+            const deletedUser = await User.deleteOne({_id: req.params.userId})
+            if (deletedUser.deletedCount == 1) 
+            {
+                await Habit.deleteMany({user_id: req.params.userId})
+                const users = await User.find()
+                return res.status(200).json(users)
+            } 
+            return res.status(404).json({retorno: 'Usuário não encontrado'})
+        } catch(error: unknown) {
+            return res.status(500).json((error as Error).message)
+        }
     }
 }
-
-export {getUser, getUsers, createUser, deleteUser}
